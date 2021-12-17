c785b965910097c8bd37877a83487d9e699a17f2
ska42 <skalyxe00@gmail.com>
2021-12-17 15:29:21 +0000

    day17 -> python

--- day17/python/day17.py
+++ day17/python/day17.py
@@ -17,13 +17,15 @@ def main():
 	x_pos = list(map(int, list(line[0].split("=")[1].split(".."))))
 	y_pos = list(map(int, list(line[1].split("=")[1].split(".."))))
 	# 200 x - 400 y (200; -200)
+	velocities = []
 	maxxx_y = 0
 	for x in range(0, width):
-		for y in range(0, height):
-			velocity = [x, y]
+		for y in range(-height, height):
+			init_velocity = [x, y]
+			velocity = init_velocity
 #			print(velocity)
 			probe = [0, 0]
-			maxx_y = 0
+			maxx_y = -1
 			max_y = 0
 			while probe[0] < width and probe[1] > -height:
 				probe[0] += velocity[0]
@@ -35,11 +37,16 @@ def main():
 				elif velocity[0] < 0:
 					velocity[0] += 1
 				velocity[1] -= 1
-				if probe[1] < height and probe[0] >= x_pos[0] and probe[0] <= x_pos[1] and probe[1] >= y_pos[0] and probe[1] <= y_pos[1]:
+				if probe[0] >= x_pos[0] and probe[0] <= x_pos[1] and probe[1] >= y_pos[0] and probe[1] <= y_pos[1]:
+					if maxx_y == -1:
+						velocities.append(init_velocity)
 					maxx_y = max_y
 			if maxxx_y < maxx_y:
 				maxxx_y = maxx_y
-	print(maxxx_y)
+	print("--- Part One ---")
+	print("Result:", maxxx_y)
+	print("--- Part Two ---")
+	print("Result:", len(velocities))
 #	print(x_pos)
 #	print(y_pos)
 #	print(array)
